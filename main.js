d33dbc8406f0f5024986b4af68afd698b13da329
KamalLahloh <kamal.lahloh@gmail.com>
2023-12-20 23:48:50 +0300

    class PaginationHelper  (done)

--- main.js
+++ main.js
@@ -247,39 +247,24 @@ class PaginationHelper {
     // returns the number of pages
     return Math.ceil(this.collection.length / this.itemsPerPage);
   }
+
   pageItemCount(pageIndex) {
     // returns the number of items on the current page. page_index is zero based.
     // this method should return -1 for pageIndex values that are out of range
 
-
-
-    // return pageIndex < this.pageCount() - 1 && pageIndex >= 0
-    //   ? this.itemsPerPage
-    //   : pageIndex === this.pageCount() - 1
-    //   ? this.collection.length % this.itemsPerPage
-    //   : -1;
-
-
     return pageIndex < this.pageCount() - 1 && pageIndex >= 0
       ? this.itemsPerPage
-      : pageIndex === this.pageCount() - 1
+      : pageIndex === this.pageCount() - 1 && pageIndex >= 0
       ? this.pageCount() % this.itemsPerPage === 0
-        ? 1
-        : this.pageCount() % this.itemsPerPage
+        ? this.itemCount() - pageIndex * this.itemsPerPage
+        : this.pageCount() < this.itemsPerPage
+        ? this.itemCount() % this.itemsPerPage === 0
+          ? this.itemsPerPage
+          : this.itemCount() % this.itemsPerPage
+        : this.itemsPerPage
       : -1;
-
-    // if (pageIndex < this.pageCount() - 1 && pageIndex >= 0) {
-    //   return this.itemsPerPage;
-    // } else if (pageIndex === this.pageCount() - 1) {
-    //   if (this.pageCount() % this.itemsPerPage === 0) {
-    //     return 1;
-    //   } else {
-    //     return this.pageCount() % this.itemsPerPage;
-    //   }
-    // } else {
-    //   return -1;
-    // }
   }
+
   pageIndex(itemIndex) {
     // determines what page an item is on. Zero based indexes
     // this method should return -1 for itemIndex values that are out of range
@@ -296,10 +281,16 @@ console.log(helper.pageItemCount(0)); // should == 4
 console.log(helper.pageItemCount(1)); // last page - should == 2
 console.log(helper.pageItemCount(2)); // should == -1 since the page is invalid
 
-console.log(helper.pageItemCount(2)); // for itemCount = 3 itemsPerPage = 1 equal 1
+// helper.pageItemCount(2);  for itemCount = 3 itemsPerPage = 1 equal 1
+// helper.pageItemCount(2);  for itemCount = 24 itemsPerPage = 10 equal 4
 
 // pageIndex takes an item index and returns the page that it belongs on
 console.log(helper.pageIndex(5)); // should == 1 (zero based index)
 console.log(helper.pageIndex(2)); // should == 0
 console.log(helper.pageIndex(20)); // should == -1
 console.log(helper.pageIndex(-10)); // should == -1
+
+
+//! /////////////////////////////////////
+console.clear();
+
