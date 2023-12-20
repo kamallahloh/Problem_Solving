0e32d8e64569535378a43623424c4f35e7181664
KamalLahloh <kamal.lahloh@gmail.com>
2023-12-20 22:56:11 +0300

    class PaginationHelper (working on)

--- main.js
+++ main.js
@@ -223,7 +223,14 @@ console.log(t.substring(5));
 
 //! /////////////////////////////////////
 console.clear();
+/* 
+For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array.
 
+The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+
+The following are some examples of how this class is used:
+
+*/
 class PaginationHelper {
   constructor(collection, itemsPerPage) {
     // The constructor takes in an array of items and a integer indicating how many
@@ -243,11 +250,42 @@ class PaginationHelper {
   pageItemCount(pageIndex) {
     // returns the number of items on the current page. page_index is zero based.
     // this method should return -1 for pageIndex values that are out of range
-    
+
+
+
+    // return pageIndex < this.pageCount() - 1 && pageIndex >= 0
+    //   ? this.itemsPerPage
+    //   : pageIndex === this.pageCount() - 1
+    //   ? this.collection.length % this.itemsPerPage
+    //   : -1;
+
+
+    return pageIndex < this.pageCount() - 1 && pageIndex >= 0
+      ? this.itemsPerPage
+      : pageIndex === this.pageCount() - 1
+      ? this.pageCount() % this.itemsPerPage === 0
+        ? 1
+        : this.pageCount() % this.itemsPerPage
+      : -1;
+
+    // if (pageIndex < this.pageCount() - 1 && pageIndex >= 0) {
+    //   return this.itemsPerPage;
+    // } else if (pageIndex === this.pageCount() - 1) {
+    //   if (this.pageCount() % this.itemsPerPage === 0) {
+    //     return 1;
+    //   } else {
+    //     return this.pageCount() % this.itemsPerPage;
+    //   }
+    // } else {
+    //   return -1;
+    // }
   }
   pageIndex(itemIndex) {
     // determines what page an item is on. Zero based indexes
     // this method should return -1 for itemIndex values that are out of range
+    return itemIndex >= 0 && itemIndex < this.itemCount()
+      ? Math.floor(itemIndex / this.itemsPerPage)
+      : -1;
   }
 }
 
@@ -257,6 +295,8 @@ console.log(helper.itemCount()); // should == 6
 console.log(helper.pageItemCount(0)); // should == 4
 console.log(helper.pageItemCount(1)); // last page - should == 2
 console.log(helper.pageItemCount(2)); // should == -1 since the page is invalid
+
+console.log(helper.pageItemCount(2)); // for itemCount = 3 itemsPerPage = 1 equal 1
 
 // pageIndex takes an item index and returns the page that it belongs on
 console.log(helper.pageIndex(5)); // should == 1 (zero based index)
