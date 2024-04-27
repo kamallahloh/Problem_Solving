b66288709a052bac69b416dc5be380917aaf3d8f
KamalLahloh <kamal.lahloh@gmail.com>
2024-04-27 15:28:04 +0300

    miniMaxSum

---
+++ HackerRank/miniMaxSum.js
@@ -0,0 +1,50 @@
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+// PrepareInterview Preparation Kits1 Month Preparation KitWeek 1Mini-Max Sum
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+  // Write your code here
+  arr.sort((a, b) => a - b);
+  const max = arr.reduce((acc, elem, i) => (i > 0 ? acc + elem : acc), 0);
+  const min = arr.reduce(
+    (acc, elem, i) => (i < arr.length - 1 ? acc + elem : acc),
+    0
+  );
+  console.log(min, max);
+}
+
+function main() {
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  miniMaxSum(arr);
+}
