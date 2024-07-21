f466dd4889c1435a80c7d045518e487491fdaa54
Kamal Lahloh <kamal.lahloh@gmail.com>
2024-07-21 10:25:16 +0300

    Codewars / noSpace

--- Tricks/main.js
+++ Tricks/main.js
@@ -34,6 +34,8 @@
 
 //! /////////////////////////////////////
 
+// array of consecutive numbers // ==> .keys() Returns an iterable of keys in the array
+
 // console.log([...new Array(10).keys()]); //  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 // console.log([...new Array(10).keys()].map((x) => x + 1)); //  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
