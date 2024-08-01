8e062ed89881fb4f9f40268d3103e120a7d2a9d4
Kamal Lahloh <kamal.lahloh@gmail.com>
2024-08-01 13:55:07 +0300

    Delphi / Mumblings

--- Delphi/CodeWars2.dpr
+++ Delphi/CodeWars2.dpr
@@ -2,12 +2,14 @@ program CodeWars2;
 
 uses
   QForms,
-  Katas2 in 'Katas2.pas' {Kata};
+  Katas2 in 'Katas2.pas' {Kata},
+  Mumblings in 'Mumblings.pas' {Mumbling};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TKata, Kata);
+  //Application.CreateForm(TKata, Kata);
+  Application.CreateForm(TMumbling, Mumbling);
   Application.Run;
 end.
