ba2b6b2a3a92ba781a5306a8e36283393ced82f9
KamalLahloh <kamal.lahloh@gmail.com>
2024-08-20 22:52:41 +0300

    sumIntervals DONE

--- Codewars/sumIntervals.js
+++ Codewars/sumIntervals.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/javascript
 // Sum of Intervals
 
-function sumIntervals(intervals) {
+/* function sumIntervals(intervals) {
   if (intervals.length === 1) return intervals[0][1] - intervals[0][0];
 
   const sortedIntervals = intervals.sort((a, b) => a[0] - b[0]);
@@ -32,6 +32,35 @@ function sumIntervals(intervals) {
   for (const interval of overLappedIntervals) {
     sum += interval[1] - interval[0];
   }
+  return sum;
+} */
+
+function sumIntervals(intervals) {
+  if (intervals.length === 0) return 0;
+
+  const sortedIntervals = intervals.sort((a, b) => a[0] - b[0]);
+
+  let tempInterval = sortedIntervals[0];
+  const overlappingIntervals = [];
+
+  for (let i = 1; i < sortedIntervals.length; i++) {
+    const interval = sortedIntervals[i];
+
+    if (interval[0] <= tempInterval[1]) {
+      tempInterval[1] = Math.max(tempInterval[1], interval[1]);
+    } else {
+      overlappingIntervals.push(tempInterval);
+      tempInterval = interval;
+    }
+  }
+
+  overlappingIntervals.push(tempInterval);
+
+  let sum = 0;
+  for (const interval of overlappingIntervals) {
+    sum += interval[1] - interval[0];
+  }
+
   return sum;
 }
 
