10afbd7f00179c1bfaa7ee4b857d35b3d8affeaa
KamalLahloh <kamal.lahloh@gmail.com>
2024-12-08 22:10:03 +0300

    Codewars C# ParseInt

--- Codewars/C#/CodeWars/Kata.cs
+++ Codewars/C#/CodeWars/Kata.cs
@@ -34,13 +34,84 @@ namespace CodeWars
             //Console.WriteLine(ToWeirdCase("Weird Casing All The Way Break The Code"));
 
             //Console.WriteLine("00:59:59 ==>", GetReadableTime(3599));
-            Console.WriteLine("23:59:59 ==>" + GetReadableTime(86399));
-            Console.WriteLine("24:00:00 ==>" + GetReadableTime(86400));
-
+            //Console.WriteLine("23:59:59 ==>" + GetReadableTime(86399));
+            //Console.WriteLine("24:00:00 ==>" + GetReadableTime(86400));
+            Console.WriteLine(ParseInt("two hundred forty-six")); // 246
+            Console.WriteLine(ParseInt("two hundred forty-six thousand")); // 246000
+            Console.WriteLine(ParseInt("seven hundred eighty-three thousand nine hundred and nineteen")); // 783919
         }
 
-        public static string GetReadableTime(int time) =>
-            $"{(int)TimeSpan.FromSeconds(time).TotalHours:D2}:{TimeSpan.FromSeconds(time).ToString(@"mm\:ss")}";
+
+
+        public static int ParseInt(string s)
+        {
+            var numbersDictionary = new Dictionary<string, int>
+            {
+                { "zero", 0 },
+                { "one", 1 },
+                { "two", 2 },
+                { "three", 3 },
+                { "four", 4 },
+                { "five", 5 },
+                { "six", 6 },
+                { "seven", 7 },
+                { "eight", 8 },
+                { "nine", 9 },
+                { "ten", 10 },
+                { "eleven", 11 },
+                { "twelve", 12 },
+                { "thirteen", 13 },
+                { "fourteen", 14 },
+                { "fifteen", 15 },
+                { "sixteen", 16 },
+                { "seventeen", 17 },
+                { "eighteen", 18 },
+                { "nineteen", 19 },
+                { "twenty", 20 },
+                { "thirty", 30 },
+                { "forty", 40 },
+                { "fifty", 50 },
+                { "sixty", 60 },
+                { "seventy", 70 },
+                { "eighty", 80 },
+                { "ninety", 90 },
+                { "hundred", 100 },
+                { "thousand", 1000 },
+                { "million", 1000000 },
+            };
+
+            var words = s.Replace(" and ", " ").Replace("-", " ").Split(' ');
+            int result = 0;
+            int current = 0;
+
+            foreach (var word in words)
+            {
+                if (numbersDictionary.ContainsKey(word))
+                {
+                    int number = numbersDictionary[word];
+                    if (number == 100 || number == 1000 || number == 1000000)
+                    {
+                        current *= number;
+                    }
+                    else
+                    {
+                        current += number;
+                    }
+
+                    if (number == 1000 || number == 1000000)
+                    {
+                        result += current;
+                        current = 0;
+                    }
+                }
+            }
+
+            result += current;
+            return result;
+        }
+
+        //public static string GetReadableTime(int time) =>
+        //    $"{(int)TimeSpan.FromSeconds(time).TotalHours:D2}:{TimeSpan.FromSeconds(time):mm\\:ss}";
 
         //return string.Format("{0:d2}:{1:d2}:{2:d2}", seconds / 3600, seconds / 60 % 60, seconds % 60);
 
